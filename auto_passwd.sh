ffee0a7fb66fbc1e296d6770489746fd7e1689b1
momoein <100838174+momoein@users.noreply.github.com>
2023-05-12 19:47:39 +0330

    Update auto_passwd.sh and dibug

--- auto_passwd.sh
+++ auto_passwd.sh
@@ -14,7 +14,7 @@ while read -r line; do
    # Check if the user exists
    if id "$username" >/dev/null 2>&1; then
       # Change the user's password
-      echo "$password" | passwd --stdin "$username"
+      echo "username:newpassword" | chpasswd
       echo "Password for user $username changed"
    else
       echo "User $username does not exist, skipping..."

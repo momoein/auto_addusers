73ef27f35fee4517569c012faace9f785495cde1
momoein <100838174+momoein@users.noreply.github.com>
2023-06-23 04:19:19 +0330

    Create user_blocker.sh
    change password of users in the expiry_users

---
+++ user_blocker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" >&2
+   exit 1
+fi
+
+# Read the usernames and passwords from a text file
+while read -r line; do
+   username=$(echo "$line" | cut -d':' -f1)
+   password=$(echo "$line" | cut -d':' -f2)
+   
+   # Check if the user exists
+   if id "$username" >/dev/null 2>&1; then
+      # Change the user's password
+      echo "$username:$password" | chpasswd
+      echo "Password for user $username changed"
+   else
+      echo "User $username does not exist, skipping..."
+   fi
+done < expiry_users.txt
+
+echo "Done."
